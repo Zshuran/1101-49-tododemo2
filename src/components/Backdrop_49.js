a802581142e429b6b47f72f42940e9a6fea96607
Zshuran <a86619355@gmail.com>
2021-10-09 19:33:05 +0800

    add <Backdrop />, <Modal_49 />

---
+++ src/components/Backdrop_49.js
@@ -0,0 +1,5 @@
+function Backdrop_49(props) {
+  return <div class="backdrop" onClick={props.onClose}></div>;
+}
+
+export default Backdrop_49;
--- src/components/Todo_49.js
+++ src/components/Todo_49.js
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal_49 from '../components/Modal_49';
+import Backdrop_49 from '../components/Backdrop_49';
+
 function Todo_49(props) {
+  const [showModal, setshowModal] = useState();
+
+  function showModalHandler() {
+    setshowModal(true);
+  }
+
+  function closeModalHandler() {
+    setshowModal(false);
+  }
   return (
     <div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick={showModalHandler}>
+            Delete
+          </button>
         </div>
       </div>
+      {showModal && <Backdrop_49 onClose={closeModalHandler} />}
+      {showModal && (
+        <Modal_49 text="Are you sure?" onClose={closeModalHandler} />
+      )}
     </div>
   );
 }
